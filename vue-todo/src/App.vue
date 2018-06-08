ce3e0a067790c11d4f58e2f88a21eebefbaa8142
RESPECTEDY <jkyoon@me.com>
2018-06-09 01:17:06 +0900

    1-1 컴포넌트 생성 및 등록하기

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+        <TodoHeader></TodoHeader>
+        <TodoInput></TodoInput>
+        <TodoList></TodoList>
+        <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+/* ES5
+var my_cmp = {
+  template: '<div>my component</div>'
+};
+
+new Vue({
+  el: '',
+  components:{
+    'my-cmp': my_cmp
+  }
+});
+
+*/
+
+// ES6
+export default {
+  components:{
+    // 컴포넌트 태그명 : 컴포넌트 내용
+    'TodoHeader': TodoHeader,
+    'TodoInput': TodoInput,
+    'TodoList': TodoList,
+    'TodoFooter': TodoFooter,
+  }
+}
+</script>
+
+<style>
+
+</style>
