b3383aa6dd797402f466aae7ff04fe56ff04bed2
hybrid <51018905+aaa2511@users.noreply.github.com>
2019-07-29 18:09:36 +0900

    1차 커밋

---
+++ todo/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <TodoHeader />
+    <TodoInput />
+    <TodoList />
+    <TodoFooter />
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader'
+import TodoInput from './components/TodoInput'
+import TodoList from './components/TodoList'
+import TodoFooter from './components/TodoFooter'
+
+export default {
+  components : {
+    'TodoHeader' : TodoHeader,
+    'TodoInput' : TodoInput,
+    'TodoList' : TodoList,
+    'TodoFooter' : TodoFooter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body{
+  background-color: #f6f6f6;
+}
+input{
+  border-style: groove;
+  width: 200px;
+}
+button{
+  border-style: groove;
+}
+.shadow{
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
+</style>
---
+++ todo/src/components/TodoList.vue
@@ -0,0 +1,29 @@
+<template>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            todoItems: []
+        }
+    },
+    created: function(){ 
+        if(localStorage.length  > 0) {
+            for(var i = 0; i < localStorage.length; i++)
+            {
+                this.todoItems.push(localStorage.key(i));
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
